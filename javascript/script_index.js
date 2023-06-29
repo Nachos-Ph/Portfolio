06283aec3ae15a4033c1072904248b496000c169
Nachos <114917599+Nachos-Ph@users.noreply.github.com>
2023-06-29 20:52:14 +0800

    Add files via upload

---
+++ javascript/script_index.js
@@ -0,0 +1,37 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  var icon = document.querySelector(".topnav .icon");
+  
+  if (x.className === "topnav") {
+    x.className += " responsive";
+    icon.classList.add("animate");
+    x.style.backgroundColor = "#04AA6D"; 
+  } else {
+    x.className = "topnav";
+    icon.classList.remove("animate");
+    x.style.backgroundColor = "#333"; 
+  }
+}
+
+
+window.addEventListener("resize", function() {
+  var x = document.getElementById("myTopnav");
+  var icon = document.querySelector(".topnav .icon");
+
+  if (window.innerWidth > 600) { 
+    x.className = "topnav"; 
+    icon.classList.remove("animate");
+    x.style.backgroundColor = "#333"; 
+  }
+
+});
+
+function goToAboutPage() {
+  window.location.href = "about.html";
+}
+
+function goToProjectsPage() {
+  window.location.href = "projects.html";
+}
+
+
