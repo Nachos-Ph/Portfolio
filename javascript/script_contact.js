efa9b7de1106f9ae1dc59f601c87fc8b9bcda7a0
Nachos <114917599+Nachos-Ph@users.noreply.github.com>
2023-06-29 23:15:16 +0800

    Add files via upload

--- javascript/script_contact.js
+++ javascript/script_contact.js
@@ -1,27 +1,26 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    var icon = document.querySelector(".topnav .icon");
-    
-    if (x.className === "topnav") {
-      x.className += " responsive";
-      icon.classList.add("animate");
-      x.style.backgroundColor = "#04AA6D"; 
-    } else {
-      x.className = "topnav";
-      icon.classList.remove("animate");
-      x.style.backgroundColor = "#333"; 
-    }
+  var x = document.getElementById("myTopnav");
+  var icon = document.querySelector(".topnav .icon");
+  
+  if (x.className === "topnav") {
+    x.className += " responsive";
+    icon.classList.add("animate");
+    x.style.backgroundColor = "#04AA6D"; 
+  } else {
+    x.className = "topnav";
+    icon.classList.remove("animate");
+    x.style.backgroundColor = "#333"; 
   }
-  
-  
-  window.addEventListener("resize", function() {
-    var x = document.getElementById("myTopnav");
-    var icon = document.querySelector(".topnav .icon");
-  
-    if (window.innerWidth > 600) { 
-      x.className = "topnav"; 
-      icon.classList.remove("animate");
-      x.style.backgroundColor = "#333"; 
-    }
-  });
-  
+}
+
+
+window.addEventListener("resize", function() {
+  var x = document.getElementById("myTopnav");
+  var icon = document.querySelector(".topnav .icon");
+
+  if (window.innerWidth > 600) { 
+    x.className = "topnav"; 
+    icon.classList.remove("animate");
+    x.style.backgroundColor = "#333"; 
+  }
+});
